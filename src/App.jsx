39aabb02755f65f1fc295534cfc72eae8452e55b
design.devanshu@gmail.com <design.devanshu@gmail.com>
2023-10-13 17:38:22 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="wrapper">
+        <h1>
+          This site is under construction{" "}
+          <span role="img" aria-label="construction">
+            🚧
+          </span>
+        </h1>
+        <p>
+          I am Devanshu, a Frontend Developer. I am currently working on this site and it will be live soon. Meanwhile you can check out my{" "}
+          <a href="https://github.com/designdotdevanshu" target="_blank" rel="noreferrer">
+            Github
+          </a>
+        </p>
+        <p>
+          If you want to contact me, you can do it through my email: <a href="mailto:design.devanshu@hmail.com">design.devanshu@hmail.com</a>
+        </p>
+        <p>
+          or you can find me on{" "}
+          <a href="https://www.linkedin.com/in/devanshu-singh-2b1b3b1b0/" target="_blank" rel="noreferrer">
+            LinkedIn
+          </a>{" "}
+          and{" "}
+          <a href="https://instagram.com/designdotdevanshu/" target="_blank" rel="noreferrer">
+            Instagram
+          </a>
+        </p>
+        <p></p>
+      </div>
+    </>
+  );
+}
+
+export default App;
